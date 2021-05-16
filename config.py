dafcf3ae88b7f6b2e3774731201a85f5d5200609
Günther Neumair <g.neumair@tuta.io>
2021-05-16 14:18:25 +0200

    fix scenario naming

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ szenario_basepath="./scenarios/"
 result_folder="./results/"
 samplerate=16000
 cpu_count=16
-szenarios = ['libri_test']
+szenarios = ['libri_test_v1.0']
 sensitivitys = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
 noise_levels = [20,10,0]
