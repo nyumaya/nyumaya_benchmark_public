a5842f386858d7087d61521405cb3c8bdce45bd8
Günther Neumair <g.neumair@tuta.io>
2021-05-17 21:38:18 +0200

    Example for creating a scenario from a folder, refactoring

--- makeSzenario.py
+++ makeSzenario.py
@@ -6,7 +6,7 @@ import os
 os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
 from config import *
-from utils import load_audio_file
+from utils import *
 
 import tensorflow as tf
 
@@ -15,7 +15,7 @@ sys.path.insert(0, nyumaya_basepath + '/python/src/')
 from libnyumaya import FeatureExtractor
 from auto_platform import default_libpath
 from random import shuffle
-
+from os.path import splitext
 
 def bytes_feature(value):
 	return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
@@ -24,8 +24,18 @@ nyumaya_libpath = os.path.join("./nyumaya_audio_recognition/python/src/",default
 lib_extractor = FeatureExtractor(nyumaya_libpath,nfft=512,melcount=40,
 	sample_rate=samplerate,lowerf=20,upperf=8000,window_len=0.03,shift=0.01)
 
+def write_example_to_record(meldata,text,writer):
+	example = tf.train.Example(
+		features=tf.train.Features(
+			feature={
+				'meldata': bytes_feature(meldata.tobytes()),
+				'utf_text': bytes_feature(text),
+			}
+		)
+	)
+	writer.write(example.SerializeToString())
 
-def make_librispeech(in_dir = None):
+def make_librispeech(in_dir):
 
 	promptlist = []
 
@@ -51,17 +61,47 @@ def make_librispeech(in_dir = None):
 			wavdata = np.asarray(wavdata, dtype = np.int16)
 			meldata = lib_extractor.signalToMel(wavdata.tobytes())
 			meldata = np.reshape(meldata, (-1,40))
-			example = tf.train.Example(
-				features=tf.train.Features(
-					feature={
-						'meldata': bytes_feature(meldata.tobytes()),
-						'utf_text': bytes_feature(text),
-					}
-				)
-			)
-			writer.write(example.SerializeToString())
+			write_example_to_record(meldata,text,writer)
 
-make_librispeech("./LibriTTS/test-clean/")
+# Take all audio files from a folder and
+# write them to a scenario. Watch out, pydub
+# might complain about long mp3 files
+def make_folder(in_dir,record_name):
+	promptlist = []
+	for root, dirs, files in os.walk(in_dir):
+			for f in files:
+				extension = splitext(f)[1].lower()
+				if(not (extension in extension_list)):
+					continue
+				pr = os.path.join(root,f)
+				promptlist.append(pr)
+				
+	shuffle(promptlist)
+
+	record_path = os.path.join(szenario_basepath,record_name)
+	with tf.io.TFRecordWriter(record_path) as writer:
+		for line in promptlist:
+			print(line)
+			sound,duration = load_audio_file(line)
+			print("Duration: {}".format(duration))
+
+			#Cut into 20 second slices
+			slices = sound[0:-1:20*1000]
+			for index,s in enumerate(slices):
+				wavdata = s.get_array_of_samples()
+				wavdata = np.asarray(wavdata, dtype = np.int16)
+				meldata = lib_extractor.signalToMel(wavdata.tobytes())
+				meldata = np.reshape(meldata, (-1,40))
+				text = "".encode('utf-8')
+				write_example_to_record(meldata,text,writer)
+
+#Uncomment to run
+#make_librispeech("./LibriTTS/test-clean/")
+
+
+#Uncomment to run
+#make_folder("./myfolder","ambient_test_v1.0.tfrecords")
+
 
 
 
