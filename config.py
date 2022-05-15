087a7779e456fe7e43d34de5c14fd64b5f0f4b4f
Günther Neumair <g.neumair@tuta.io>
2022-05-15 11:56:24 +0200

    Update for V3.0

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-nyumaya_basepath="./nyumaya_audio_recognition/"
+nyumaya_basepath="./nyumaya_audio_recognition"
+nyumaya_modelpath="./nyumaya_audio_recognition/models/Hotword"
 keyword_folder="./keywords/"
 noise_folder_list = ["./demand/"]
 szenario_basepath="./scenarios/"
 result_folder="./results/"
 samplerate=16000
-cpu_count=16
-szenarios = ['libri_test_v1.0','ambient_test_v1.0']
-sensitivitys = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+cpu_count=32
+szenarios = ['libri_test_v3.0','ambient_test_v3.0','radio_tv_test_v3.0','car_bike_test_v3.0']
+sensitivitys = [0.3,0.4,0.5,0.6,0.7,0.8,0.9]
 noise_levels = [20,10,0]
