a0177846c84491c8a585c203c1fac94fedca9222
Günther Neumair <g.neumair@tuta.io>
2022-05-15 11:53:18 +0200

    Add scenarios for V3.0

--- download_scenarios.sh
+++ download_scenarios.sh
@@ -1,2 +1,5 @@
-wget -P scenarios nyumaya.com/benchmark/libri_test_v1.0.tfrecords
-wget -P scenarios nyumaya.com/benchmark/ambient_test_v1.0.tfrecords
+wget -P scenarios nyumaya.com/benchmark/libri_test_v3.0.tfrecords
+wget -P scenarios nyumaya.com/benchmark/ambient_test_v3.0.tfrecords
+wget -P scenarios nyumaya.com/benchmark/car_bike_test_v3.0.tfrecords
+wget -P scenarios nyumaya.com/benchmark/radio_tv_test_v3.0.tfrecords
+
